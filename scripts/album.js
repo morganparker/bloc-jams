119ebe3acddbe4afdb025f8250caeb6cb005f5ff
Morgan Parker <morgancannino@gmail.com>
2016-08-10 13:17:26 -0400

    got checkpoint 26 to work

--- scripts/album.js
+++ scripts/album.js
@@ -1,35 +1,34 @@
 var albumPicasso = {
- title: 'The Colors',
- artist: 'Pablo Picasso',
- label: 'Cubism',
- year: '1881',
- albumArtUrl: 'assets/images/album_covers/01.png',
- songs: [
- +         { title: 'Blue', duration: '4:26' },
- +         { title: 'Green', duration: '3:14' },
- +         { title: 'Red', duration: '5:01' },
- +         { title: 'Pink', duration: '3:21'},
- +         { title: 'Magenta', duration: '2:15'}
- +     ]
- + };
- + 
- + // Another Example Album
-var albumMarconi = {
- title: 'The Telephone',
- artist: 'Guglielmo Marconi',
- label: 'EM',
- year: '1909',
- albumArtUrl: 'assets/images/album_covers/20.png',
- songs: [
- +         { title: 'Hello, Operator?', duration: '1:01' },
- +         { title: 'Ring, ring, ring', duration: '5:01' },
- +         { title: 'Fits in your pocket', duration: '3:21'},
- +         { title: 'Can you hear me now?', duration: '3:14' },
- +         { title: 'Wrong phone number', duration: '2:15'}
- +     ]
- + };
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
 
-<<<<<<< HEAD
  var albumBandOfHorses = {
      title: 'Infinite Arms',
      artist: 'Band of Horses',
@@ -45,11 +44,10 @@ var albumMarconi = {
      ]
  };
 
-  var createSongRow = function(songNumber, songName, songLength) {
->>>>>>> master
+var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -81,8 +79,26 @@ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
      }
  };
  
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     songListContainer.addEventListener('mouseover', function(event) {
+          if (event.target.parentElement.className === 'album-view-song-item') {
+              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
      
      var albums = [albumPicasso, albumMarconi, albumBandOfHorses];
      
