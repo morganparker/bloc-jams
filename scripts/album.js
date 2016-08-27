93be8641e1c45b685dfde93f5910a873a60cf870
morganparker <morgancannino@gmail.com>
2016-08-27 12:02:08 -0400

    attempting assignment 27

--- scripts/album.js
+++ scripts/album.js
@@ -1,18 +1,26 @@
 var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
+    if (element.parent().length = 0) {
+        alert("No parent found");
+    }
+    else if (element.parent() != element.querySelector('.song-item-number') {
+          alert("No parent found with that class name");   
+    }
+    else {
+        switch (element.className) {
+            case 'album-song-button':
+            case 'ion-play':
+            case 'ion-pause':
+                return findParentByClassName(element, 'song-item-number');
+            case 'album-view-song-item':
+                return element.querySelector('.song-item-number');
+            case 'song-item-title':
+            case 'song-item-duration':
+                return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+            case 'song-item-number':
             return element;
         default:
             return;
+        }
     }  
 };
 
